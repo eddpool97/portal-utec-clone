a13c98bc58e22ea60f7ec276461f29a7be36c74f
eddpool97 <78087019+eddpool97@users.noreply.github.com>
2026-02-19 15:58:03 +0000

    package.json and express and jest npm install executed

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+// Método 1: Servir un archivo específico
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html')); // [5, 10]
+});
+
+// Método 2: Servir una carpeta de archivos estáticos (opcional, mejor para imágenes/css)
+// app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
